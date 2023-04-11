6d42c73b948da91e4c6440545ff432c4254bb54e
Linda Paz <lindacsndelapaz@gmail.com>
2023-04-11 18:31:34 -0300

    first commit

---
+++ src/condicionales/Condicionales.java
@@ -0,0 +1,16 @@
+
+package condicionales;
+
+
+public class Condicionales {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 1;
+        
+        if( num2 < num1){
+            System.out.println("El numero 2 es mayor al numero 1");
+        }
+    }
+    
+}
