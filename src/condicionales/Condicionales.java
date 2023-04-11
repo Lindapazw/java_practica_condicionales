517fd600413cf84a7604cbff563b758a54e7d89a
Linda Paz <lindacsndelapaz@gmail.com>
2023-04-11 18:49:17 -0300

    add if + else

--- src/condicionales/Condicionales.java
+++ src/condicionales/Condicionales.java
@@ -5,12 +5,28 @@ package condicionales;
 public class Condicionales {
 
     public static void main(String[] args) {
+        
+        // if 
         int num1 = 5;
         int num2 = 1;
         
         if( num2 < num1){
             System.out.println("El numero 2 es mayor al numero 1");
         }
+        
+        // if + else
+        int num3 = 1;
+        int num4 = 10;
+        
+        if( num3 > num4) {
+            System.out.println("El numero 3 es mayor al numero 4 ");
+        }
+        else {
+            System.out.println("El numero 4 es mayor");
+        }
+        
+        
+        
     }
     
 }
