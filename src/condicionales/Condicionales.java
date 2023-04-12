417fd1f7c77001e56663f792b585d352d56120b5
Linda Paz <lindacsndelapaz@gmail.com>
2023-04-12 11:33:02 -0300

    add switch

--- src/condicionales/Condicionales.java
+++ src/condicionales/Condicionales.java
@@ -41,6 +41,33 @@ public class Condicionales {
                 System.out.println("El numero 11 es mayor al numero 10");
             }
         }
+        
+        // switch - multiples sentencias a partir de una condicion
+        // ¿Cual es el dia elegido?
+        int dia = 5;
+        String nombreDia;
+        
+        switch (dia) {
+            case 1: nombreDia="Lunes";
+            break;
+            case 2: nombreDia="Martes";
+            break;
+            case 3: nombreDia="Miercoles";
+            break;
+            case 4: nombreDia ="Jueves";
+            break;
+            case 5: nombreDia = "Viernes";
+            break;
+            case 6: nombreDia = "Sabado";
+            break;
+            case 7: nombreDia = "Domingo";
+            break;
+            default: nombreDia ="Numero de dia invalido";
+            break;
+        }
+        System.out.println("El dia de la semana elegido es: " + nombreDia);
+        
+        
     }
     
 }
